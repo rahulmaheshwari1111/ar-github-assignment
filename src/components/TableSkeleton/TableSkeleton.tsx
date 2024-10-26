10e2e227382cdce34d13bd2e5545656568de4461
Rahul Maheshwari <rahulmaheshwari@Rahuls-MacBook-Air.local>
2024-10-26 17:18:51 +0530

    feat: added table skelton

---
+++ src/components/TableSkeleton/TableSkeleton.tsx
@@ -0,0 +1,48 @@
+// components/TableSkeleton.tsx
+import React from 'react';
+import { Group, Table, Skeleton } from '@mantine/core';
+
+interface TableSkeletonProps {
+  rows?: number;
+  columns?: number;
+}
+
+export const TableSkeleton: React.FC<TableSkeletonProps> = ({ rows = 10, columns = 6 }) => {
+  return (
+    <Group m="xl">
+      <Table
+        stickyHeader
+        highlightOnHover
+        withTableBorder
+        withColumnBorders
+        stickyHeaderOffset={60}
+        horizontalSpacing="md"
+        verticalSpacing={20}
+        c={"cyan"}
+        bg="white"
+        miw={"80vw"}
+      >
+        <Table.Thead>
+          <Table.Tr>
+            {Array.from({ length: columns }).map((_, index) => (
+              <Table.Th key={index}>
+                <Skeleton height={20} radius="sm" width="100%" />
+              </Table.Th>
+            ))}
+          </Table.Tr>
+        </Table.Thead>
+        <Table.Tbody>
+          {Array.from({ length: rows }).map((_, rowIndex) => (
+            <Table.Tr key={rowIndex}>
+              {Array.from({ length: columns }).map((_, colIndex) => (
+                <Table.Td key={colIndex}>
+                  <Skeleton height={30} radius="sm" width={`${50 + Math.random() * 40}%`} />
+                </Table.Td>
+              ))}
+            </Table.Tr>
+          ))}
+        </Table.Tbody>
+      </Table>
+    </Group>
+  );
+};
